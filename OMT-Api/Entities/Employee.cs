cb48835b674cc7e376953588c8a9093227d615b9
rizbons <rizbo.ns.bk@gmail.com>
2022-11-10 08:56:35 +0100

    Entity - Employee

---
+++ OMT-Api/Entities/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OMT_Api.Entities
+{
+    public class Employee
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required, MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required, MaxLength(50)]
+        public string LastName { get; set; }
+
+        [Required, MaxLength(50)]
+        public string Email { get; set; }
+
+        [Required, MinLength(6), MaxLength(50)]
+        public string Password { get; set; }
+
+        [Required, MinLength(8), MaxLength(8)]
+        public string EmployeeId { get; set; }
+
+        [Required]
+        public Role Role { get; set; }
+    }
+
+    public enum Role
+    {
+        agent, admin
+    }
+}
