e83112c469cc19b5559e713174d09b90bc1f98db
rizbons <rizbo.ns.bk@gmail.com>
2022-11-15 11:15:02 +0100

    PATCH, DELETE

--- OMT-Api/Controllers/CustomersController.cs
+++ OMT-Api/Controllers/CustomersController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using OMT_Api.Data;
@@ -37,6 +38,43 @@ namespace OMT_Api.Controllers
             await _context.Customers.AddAsync(customer);
             await _context.SaveChangesAsync();
             return customer == null ? BadRequest() : CreatedAtAction(nameof(GetById), new { id = customer.Id }, customer);
+        }
+
+        // TO DO TEST
+        [HttpPatch("{id}"), Authorize]
+        public async Task<IActionResult> Update(Guid id, [FromBody] JsonPatchDocument<Customer> customerDoc)
+        {
+            if (customerDoc == null)
+            {
+                return BadRequest();
+            }
+
+            var customer = await _context.Customers.FindAsync(id);
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            customerDoc.ApplyTo(customer);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // TO DO TEST
+        [HttpDelete("{id}"), Authorize]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var customerToDelete = await _context.Customers.FindAsync(id);
+            if (customerToDelete == null)
+            {
+                return NotFound();
+            }
+            _context.Customers.Remove(customerToDelete);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
