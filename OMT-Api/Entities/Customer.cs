b1fff14d04b6e18f46a2f931c1078dbd81a8db5a
rizbons <rizbo.ns.bk@gmail.com>
2022-11-15 06:02:55 +0100

    Customer Entity

---
+++ OMT-Api/Entities/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OMT_Api.Entities
+{
+    public class Customer
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required, MaxLength(50)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required, MaxLength(50)]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required, MaxLength(50)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required, MaxLength(10)]
+        public string Phone { get; set; } = string.Empty;
+
+        [Required, MaxLength(50)]
+        public string Address { get; set; } = string.Empty;
+
+        [Required, MaxLength(50)]
+        public string City { get; set; } = string.Empty;
+
+        [Required, MaxLength(50)]
+        public string State { get; set; } = string.Empty;
+
+        [Required, MaxLength(5)]
+        public string Zip { get; set; } = string.Empty;
+    }
+}
